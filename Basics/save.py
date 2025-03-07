5e98ec20a760325cb937e917ad73b50cb288100b
Linaeus14 <titodqrmawan123@gmail.com>
2025-03-07 10:14:38 +0800

    update: save

--- Basics/save.py
+++ Basics/save.py
@@ -22,35 +22,34 @@ font = pygame.font.SysFont("Verdana", 100)
 save_file = "Basics/assets/save.json"
 
 # Fungsi untuk menyimpan nomor ke file
-def save_number(number):
+def save_number(number, name):
     with open(save_file, "w") as penyimpanan:
-        json.dump({"number": number}, penyimpanan)
+        json.dump({"number": number, "name": name}, penyimpanan)
 
 # Fungsi untuk memuat nomor dari file
 def load_number():
     if os.path.exists(save_file):
         with open(save_file, "r") as penyimpanan:
-            data = json.load(penyimpanan)
-            return data.get("number", 0)
+            nilai = json.load(penyimpanan)
+            return [nilai.get("number", 0), nilai.get("name", "Tidak Terbaca")]
     return 0
 
-# Inisialisasi nomor
-number = load_number()
 
-# Fungsi untuk menggambar nomor di layar
-def draw_number(screen, number):
-    screen.fill(black)
-    text_surface = font.render(str(number), True, white)
-    text_rect = text_surface.get_rect(center=(screen_width // 2, screen_height // 2))
-    screen.blit(text_surface, text_rect)
-    pygame.display.flip()
+# Inisialisasi data
+data = load_number()
+number = data[0]
+name = data[1]
 
-# Draw the initial number
-draw_number(screen, number)
+
+def draw(screen, text, x, y):
+    text_surface = font.render(str(text), True, white)
+    screen.blit(text_surface, (x, y))
+
 
 # Main loop
 running = True
 while running:
+    screen.fill(black)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             save_number(number)
@@ -60,8 +59,9 @@ while running:
                 number += 1
             if event.key == pygame.K_DOWN and number > 0:
                 number -= 1
-            draw_number(screen, number)
-
+    draw(screen, number, 300, 200)
+    draw(screen, name, 300, 300)
+    pygame.display.flip()
     clock = pygame.time.Clock()
     clock.tick(30)
 
